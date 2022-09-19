d7f0b7b387b27c4203ace3d6fbf512dff1bef6ef
cogdotnet1685 <cogdotnet1685@CTSDOTNET803>
2022-09-19 11:10:39 +0530

    Add project files.

---
+++ AzuretriggersFunctions/Models/CreateTaskModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzuretriggersFunctions.Models
+{
+    public class CreateTaskModel
+    {
+        public DateTime CreatedOn { get; set; }
+        public string Description { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
